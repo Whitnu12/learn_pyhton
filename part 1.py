b46a3ef022f29cc46525c25ce760ccfe52edd78f
Whitnu12 <whitnu@gmail.com>
2022-09-24 00:15:18 +0700

    new learn

---
+++ part 1.py
@@ -0,0 +1,17 @@
+from re import A
+
+
+print('whitnuu ganteng')
+
+nama_depan = 'Whitnu'
+namaBelakang = 'Nastain'
+
+print(nama_depan + namaBelakang)
+
+
+a,b,c = 1,(2+2),'titit'
+
+print(a, c) #beda tipe data pake ","
+print(b + a) #sama tipe data boleh pake aritmatika
+print(c + ' samping')
+
